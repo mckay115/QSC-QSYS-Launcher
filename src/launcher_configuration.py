45a9d72b36f001ac3ea5dcad68bf3de911da4e0e
Zach Lisko <zacharylisko@gmail.com>
2021-11-29 22:27:08 -0600

    added config logic

--- src/launcher_configuration.py
+++ src/launcher_configuration.py
@@ -1,13 +1,17 @@
 import json
 import os
+import datetime
 
 configuration_information = {}
 
+def write_json(new_data, filename='config'):
+    with open('config', 'r') as file:
+        
+
 def create_configuration():
-    configuration_information = {}
-    configuration_information['projects'] = {'project_name': 'test_project', 'project_directory': 'C:\\Users\\zlisko\\OneDrive - Lamacchia Group\\Desktop\\qsys-catcher\\QSC-QSYS-Launcher\\template_design.qsys'}
-    with open('config', 'w') as f:
-        json.dump(configuration_information, f)
+    data = {'configuration_creation_timestamp':str(datetime.datetime.now())}
+    header = 'settings'
+    write_json(data, header)
 
 def check_for_configuration():
     config_location = 'config'
@@ -16,12 +20,26 @@ def check_for_configuration():
         config = json.load(config)
         print('Configruation file found.')
         print(config)
+        return config
     else:
         print('Configuration file not found.')
         create_configuration()
 
 
+def add_project_to_configuration(project_name='template_project', project_directory='C:\\Users\\zlisko\\OneDrive - Lamacchia Group\\Desktop\\qsys-catcher\\QSC-QSYS-Launcher\\template_design.qsys', project_number=''):
+    project_information = {
+            'project_name': project_name, 
+            'project_directory': project_directory, 
+            'project_number': project_number, 
+            'timestamp': str(datetime.datetime.now())
+            }
+    write_json(project_information)
+
 
 
 if __name__ == "__main__":
-    check_for_configuration()
+    write_json({'test': 'testing'})
+    # check_for_configuration()
+    # add_project_to_configuration(project_name='tester', project_number='181820')
+    # add_project_to_configuration(project_name='tester1', project_number='18567')
+    # add_project_to_configuration(project_name='tester2', project_number='184242')

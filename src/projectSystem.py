b85d4327e12fef2570009cc0dcd7a50cb9233f90
Zach Lisko <zacharylisko@gmail.com>
2022-09-25 13:52:55 -0500

    updated projectSystem

--- src/projectSystem.py
+++ src/projectSystem.py
@@ -1,11 +1,13 @@
 import os
 
 
+rootProjectDir = "QSYS-Projects"
+
 class Project:
     #initilization of the project with the name and the path
     def __init__(self, name, path):
         self.name = name
-        self.path = path
+        self.path = f'{path}'
         self.files = []
         self.folders = []
 
@@ -17,3 +19,17 @@ class Project:
             os.mkdir(folder)
         for file in self.files:
             open(file, 'w')
+        self.createProjectReadme()
+
+    def createProjectReadme(self):
+        with open('README.md', 'w') as readme:
+            readme.write(f'# {self.name}\n')
+            readme.write(f'Path: {self.path}\n')
+            readme.write(f'Files: {self.files}\n')
+            readme.write(f'Folders: {self.folders}\n')
+
+if __name__ == "__main__":
+    project = Project("ProjectName-Test", "ProjectName-Test-Milwaukee-WI")
+    project.folders = ["resources", ".backup-rev"]
+    project.files = ["testproject.qsys", "Readme.md"]
+    project.createProject()
